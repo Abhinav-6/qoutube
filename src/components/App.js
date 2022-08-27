8ad3a60a38ca97016c703aabbe6aa9ddd2c06933
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-08-27 09:22:00 +0530

    Added all route elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Feed from "./Feed";
+import Navbar from "./Navbar";
+import ChannelDetail from "./ChannelDetail";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
 function App() {
   return (
     <BrowserRouter>
-      <Box sx={{backgroundColor: "#000"}}>
-        Navbar
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<Feed />} />
-          <Route path="/video/:id" element={<Feed />} />
-          <Route path="/channel/:id" element={<Feed />} />
-          <Route path="/search/:searchTerm" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
         </Routes>
       </Box>
     </BrowserRouter>
