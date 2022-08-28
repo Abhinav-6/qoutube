9ed7dc742c067877095f28083001df1eca9a23a3
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-08-28 15:42:42 +0530

    Completed searchfeed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Search } from "@mui/icons-material";
+import { Paper, IconButton } from "@mui/material";
 
 export default function SearchFeed() {
   return (
-    <div>SearchFeed</div>
-  )
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        mr: { sm: 5 },
+      }}
+    >
+      <input type="search" placeholder="Search..." className="search-bar" style={{border: "none"}} />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
 }
